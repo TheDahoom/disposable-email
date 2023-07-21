016f94a3830a3104bc5b5d2b1a33cac61d4dff31
Dahoom <Rozimo56@gmail.com>
2023-07-21 04:32:22 +0300

    updated disposable

--- disposable.d.ts
+++ disposable.d.ts
@@ -1,2 +1,13 @@
+// Type definitions for disposable-email 0.2
+// Project: https://github.com/disposable/disposable#readme
+// Definitions by: Zeeshan Ahmad <https://github.com/ziishaned>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+
+/**
+ * Validates if a domain or email is disposable.
+ * @param {string} domainOrEmail - The domain or email to validate.
+ * @param {function} callback - The optional callback function to execute after validation.
+ * @returns {boolean} - Returns true if the domain or email is not disposable, false otherwise.
+ */
 export function validate(domainOrEmail: string): boolean;
 export function validate(domainOrEmail: string, callback: (error: null, isValid: boolean) => void): void;
